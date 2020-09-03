a0f68c84de2bab5b356c96e370d1778c6e991d41
Pikachuxxxx <phani.s2909@icloud.com>
2020-09-03 14:13:30 +0530

    feat:added buffers and vertex arrays to buffers namespace under graphics

---
+++ fireworks-core/src/graphics/buffers/indexbuffer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+namespace fireworks { namespace graphics {
+
+    class IndexBuffer
+    {
+    private:
+        GLuint m_BufferID;
+        GLuint m_Count;
+    public:
+        IndexBuffer(GLushort* data, GLsizei count);
+
+        void bind() const;
+        void unbind() const;
+
+        inline GLuint getCount() const { return m_Count; }
+    };
+
+} }
