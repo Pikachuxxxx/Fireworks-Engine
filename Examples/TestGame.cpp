b351379f99f26ca352e982225aea9139f4a46e77
Pikachuxxxx <phani.s2909@icloud.com>
2020-09-16 20:48:16 +0530

    new branch for future development and testing (Highly unstable branch)

--- Examples/TestGame.cpp
+++ Examples/TestGame.cpp
@@ -19,6 +19,7 @@ public:
     void init() override
     {
         window = createWindow("Test Game Example : Fireworks Engine", 800, 600);
+        
         layer = new Layer(new BatchRenderer2D(),
                           new Shader("../Fireworks-core/src/shaders/basic.vert", "../Fireworks-core/src/shaders/basic.frag"),
                           mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f , 1.0f));
