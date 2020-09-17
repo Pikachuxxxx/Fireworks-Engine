e9ed6a160990d1b1e2ebe009ef7b4cb189d17861
Pikachuxxxx <phani.s2909@icloud.com>
2020-09-17 11:00:22 +0530

    feat: Added Example Template

---
+++ Examples/ExampleTemplate.cpp
@@ -0,0 +1,50 @@
+#include <fireworks/fireworks.h>
+
+using namespace fireworks;
+
+class ExampleTemplate : public Fireworks
+{
+private:
+    Window* window;
+public:
+    ExampleTemplate()
+    {
+
+    }
+
+    ~ExampleTemplate()
+    {
+
+    }
+
+    // Runs once per initialisation
+    void init() override
+    {
+        window = createWindow("Example Template", 800, 600);
+    }
+
+    // Runs once per second
+    void tick() override
+    {
+
+    }
+
+    // Runs 60 times per second
+    void update() override
+    {
+
+    }
+
+    // Runs as fast as possible
+    void render() override
+    {
+
+    }
+};
+
+int main()
+{
+    ExampleTemplate game;
+    game.start();
+    return 0;
+}
