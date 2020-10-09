4a2141998192d5794752f2f6e9f3cce0a1b3d867
Pikachuxxxx <phani.s2909@icloud.com>
2020-10-09 20:54:39 +0530

    feat: Added sprite sheet animations and extended the Sprite API

--- Fireworks Engine/Fireworks Core/src/graphics/renderables/renderable2d.h
+++ Fireworks Engine/Fireworks Core/src/graphics/renderables/renderable2d.h
@@ -22,30 +22,26 @@ namespace fireworks { namespace graphics {
     {
     protected:
 		maths::vec2                         m_Size;
-        maths::vec2                         m_SpriteSheetDimension;
 		maths::vec3                         m_Position;
 		maths::vec4                         m_Color;
         mutable std::vector<maths::vec2>    m_UV;
-		uint32_t                            m_Frame;
 		Texture*                            m_Texture;
     public:
         Renderable2D(maths::vec3 position, maths::vec2 size, maths::vec4 color)
-            : m_Position(position), m_Size(size), m_Color(color), m_Texture(nullptr), m_SpriteSheetDimension(maths::vec2(1, 1)), m_Frame(1)
+            : m_Position(position), m_Size(size), m_Color(color), m_Texture(nullptr)
         { 
-            //setUVDefaults();
-            setSpriteSheetUV();
+            setUVDefaults();
         }
 
         virtual ~Renderable2D() { }
 
         virtual void submit(Renderer2D* renderer) const
         {
-            setSpriteSheetUV();
             renderer->submit(this);
         }
 
-        inline const maths::vec3& getPosition() const { return m_Position; }
         inline const maths::vec2& getSize() const { return m_Size; }
+        inline const maths::vec3& getPosition() const { return m_Position; }
         inline const maths::vec4& getColor() const { return m_Color; }
         inline const std::vector<maths::vec2>& getUV() const { return m_UV; }
         inline const GLuint getTID() const { return m_Texture == nullptr ? 0 : m_Texture->getID(); }
@@ -58,20 +54,6 @@ namespace fireworks { namespace graphics {
 			m_UV.push_back(maths::vec2(0, 1));  // Top Left
             m_UV.push_back(maths::vec2(1, 1));  // Top Right
             m_UV.push_back(maths::vec2(1, 0));  // Bottom Right
-        }
-        void setSpriteSheetUV() const
-        {
-            if (m_Texture == nullptr)
-                return;
-
-			m_UV.clear();
-            // TODO: Map the 1D frame index to a row major 2D texture sheet atlas matrix
-			float x = m_Frame - 1.0f, y = 0;
-			float frameWidth = m_Texture->getWidth() / m_SpriteSheetDimension.x, frameHeight = m_Texture->getHeight() / m_SpriteSheetDimension.y;
-			m_UV.push_back(maths::vec2((   x    * frameWidth) / m_Texture->getWidth(), (   y      * frameHeight) / m_Texture->getHeight())); // Bottom Left
-			m_UV.push_back(maths::vec2((   x    * frameWidth) / m_Texture->getWidth(), ((y + 1)   * frameHeight) / m_Texture->getHeight())); // Top Left
-			m_UV.push_back(maths::vec2(((x + 1) * frameWidth) / m_Texture->getWidth(), ((y + 1)   * frameHeight) / m_Texture->getHeight())); // Top Right
-			m_UV.push_back(maths::vec2(((x + 1) * frameWidth) / m_Texture->getWidth(), (   y      * frameHeight) / m_Texture->getHeight())); // Bottom Right
         }
     };
 
