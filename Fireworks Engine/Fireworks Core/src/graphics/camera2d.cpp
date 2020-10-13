3edc27bac734f30a883aa6a5497e554aff80c9be
Pikachuxxxx <phani.s2909@icloud.com>
2020-10-13 18:30:04 +0530

    feat :  Added Camera2D component

---
+++ Fireworks Engine/Fireworks Core/src/graphics/camera2d.cpp
@@ -0,0 +1,27 @@
+#include "camera2d.h"
+
+namespace fireworks { namespace graphics {
+	
+	Camera2D::Camera2D(maths::vec2 viewXBounds, maths::vec2 viewYBounds)
+		: m_ProjectionMatrix(maths::mat4::orthographic(viewXBounds.x, viewXBounds.y, viewYBounds.x, viewYBounds.y, -1.0f, 1.0f))
+	{
+
+	}
+
+	void Camera2D::update()
+	{
+
+	}
+
+	void Camera2D::updateViewMatrix()
+	{
+		using namespace maths;
+
+		mat4 transform = mat4::translation(m_Position) * mat4::rotation(m_Rotation, maths::vec3(0.0f, 0.0f, 1.0f));
+
+		m_ViewMatrix = transform.invert();
+
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+	}
+
+} }
