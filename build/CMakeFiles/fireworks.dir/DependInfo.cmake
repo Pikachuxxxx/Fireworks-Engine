4efbbe194ef7f01a2832a380a27ce31f29122d65
Pikachuxxxx <phani.s2909@icloud.com>
2020-10-29 16:32:21 +0530

    feat:Added MacOS specific cmake build file and changes some examples to work with mac as well

--- build/CMakeFiles/fireworks.dir/DependInfo.cmake
+++ build/CMakeFiles/fireworks.dir/DependInfo.cmake
@@ -4,26 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/audio/audioclip.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/audio/audioclip.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/Shader.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/Shader.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/Window.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/Window.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/batchrenderer2d.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/batchrenderer2d.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/buffers/buffer.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/buffers/buffer.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/buffers/framebuffer.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/buffers/framebuffer.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/buffers/indexbuffer.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/buffers/indexbuffer.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/buffers/renderbuffer.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/buffers/renderbuffer.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/buffers/vertexarray.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/buffers/vertexarray.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/camera2d.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/camera2d.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/instancerenderer2d.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/instancerenderer2d.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/layers/group.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/layers/group.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/layers/layer.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/layers/layer.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/renderables/label.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/renderables/label.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/renderables/sprite.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/renderables/sprite.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/graphics/texture.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/graphics/texture.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/maths/mat4.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/maths/mat4.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/maths/vec2.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/maths/vec2.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/maths/vec3.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/maths/vec3.cpp.o"
-  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Fireworks Core/src/maths/vec4.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/fireworks.dir/Fireworks_Engine/Fireworks_Core/src/maths/vec4.cpp.o"
+  "/Users/phanisrikar/Desktop/Fireworks-Engine/Fireworks Engine/Sandbox/Sandbox.cpp" "/Users/phanisrikar/Desktop/Fireworks-Engine/build/CMakeFiles/Fireworks.dir/Fireworks_Engine/Sandbox/Sandbox.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
@@ -37,6 +18,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../Dependencies/BZIP2"
   "../Dependencies/OpenAL/include"
   "../Fireworks Engine/Sandbox/Test Dependencies"
+  "../Fireworks Engine/Fireworks Core"
   )
 
 # Targets to which this target links.
