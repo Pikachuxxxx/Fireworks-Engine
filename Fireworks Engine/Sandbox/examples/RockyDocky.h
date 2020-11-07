fd3c5df8f5caba7b14818bc4f4e68d0c6aa040ce
Pikachuxxxx <phani.s2909@icloud.com>
2020-11-07 10:33:57 +0530

    Refactored build system to work on mac after adding 2D physics

--- Fireworks Engine/Sandbox/examples/RockyDocky.h
+++ Fireworks Engine/Sandbox/examples/RockyDocky.h
@@ -29,11 +29,12 @@ private:
 private:
 	friend void ImGuiEnableDocking(bool* p_open);
 public:
-	RockyDocky() : playerVelocity(0.4f), jumpImpulse(30.0f), jumpTime(50.0f), currentJumpTime(0.0f), EditorGUI()
+	RockyDocky() : playerVelocity(3.4f), jumpImpulse(30.0f), jumpTime(50.0f), currentJumpTime(0.0f), EditorGUI()
 	{
 		window = createWindow(" Example", 800, 600);
 		camera = new Camera2D(mat4::orthographic(-160.0f, 160.0f, -120.0f, 120.0f, -1.0f, 1.0f));
 		InitGUI(window);
+#if(_WIN32)
 		Shader* basicShader = new Shader(".\\shaders\\basic.vert", ".\\shaders\\basic.frag");
 		Shader* playerShader = new Shader(".\\shaders\\basic.vert", ".\\shaders\\basic.frag");
 		Shader* sickShader = new Shader(".\\shaders\\basic.vert", ".\\shaders\\sick.frag");
@@ -67,7 +68,44 @@ public:
 
 		Font* font = new Font(".\\resources\\fonts\\SpaceQuest.ttf", 24);
 		fpsLabel = new Label(" ", vec3(20, 550, 0.5), vec3(1, 1, 1), *font);
-	
+#elif(__APPLE__)
+        Shader* basicShader = new Shader("./shaders/basic.vert", "./shaders/basic.frag");
+        Shader* playerShader = new Shader("./shaders/basic.vert", "./shaders/basic.frag");
+        Shader* sickShader = new Shader("./shaders/basic.vert", "./shaders/sick.frag");
+
+
+        InstanceRenderer2D* instancer = new InstanceRenderer2D(camera);
+        BatchRenderer2D* batcher = new BatchRenderer2D(camera, basicShader);
+        BatchRenderer2D* platform_batcher = new BatchRenderer2D(camera, sickShader);
+
+        defaultLayer = new Layer(batcher);
+        platformLayer = new Layer(platform_batcher);
+        playerLayer = new Layer(instancer);
+
+        Texture* bgTex = new Texture("./resources/rockydocky/bg.png");
+        Sprite* bg = new Sprite(vec3(0, 0, 0), vec2(400, 260), bgTex);
+
+        Texture* idleTex = new Texture("./resources/rockydocky/idle.png");
+        Texture* jumpTex= new Texture("./resources/rockydocky/jump.png");
+        rocky = new Sprite(vec3(-20, 40, 0), vec2(20, 20), playerShader, idleTex, vec2(2, 1));
+        rockyRB = new RigidBody2D(1.0f, 0.2f, RigidBodyType::Dynamic);
+        rockyRB->gravityScale = 1.0f;
+        rocky->AddComponent<RigidBody2D>(rockyRB);
+
+
+        Texture* platformTexture = new Texture("./resources/rockydocky/Design 2r.png");
+        Sprite* platform = new Sprite(vec3(-30, -60, 0), vec2(80, 10), platformTexture);
+        platformRB = new RigidBody2D(1.0f, 0.2f, RigidBodyType::Static);
+        platformRB->gravityScale = 0;
+        platformRB->fixedRotation = true;
+        platform->AddComponent<RigidBody2D>(platformRB);
+
+        Font* font = new Font("./resources/fonts/SpaceQuest.ttf", 24);
+        fpsLabel = new Label(" ", vec3(20, 550, 0.5), vec3(1, 1, 1), *font);
+
+#endif
+
+
 		defaultLayer->add(bg);
 		playerLayer->add(rocky);
 		platformLayer->add(platform);
@@ -115,7 +153,7 @@ public:
 		playerLayer->render();
 		defaultLayer->render();
 
-		InitRenderingGUI(); 
+		InitRenderingGUI();
 	}
 
 	void RenderGUI()
@@ -153,4 +191,3 @@ public:
 			currentJumpTime = 0;
 	}
 };
-
